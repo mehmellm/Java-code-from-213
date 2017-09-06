5d7abf44c536483c384e448bb47bb426d6472a77
Mehmel <mehmell@canisius.edu>
2017-09-06 19:55:52 -0400

    Second commit for Lab2

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -4,12 +4,12 @@ public class Main
 {
 	public static void main(String[] args )
 	{
-		String prompt = "Please enter some text: ";
+		String prompt = "Please enter a mathematical expression ( addition or substraction only, q to quit): ";
 		Scanner input = new Scanner(System.in );
 		
 		while(true)
 		{
-			System.out.print( prompt );;
+			System.out.print( prompt );
 			String in = input.nextLine();
 			
 			// removing whitespace
@@ -18,27 +18,48 @@ public class Main
 			// comparing entire string
 			if( in.equals( "q" ) ) // or .equalsIgnoreCase()
 			{
-				System.out.println( "You found the q ");
+				System.out.println( "Goodbye!!! ");
 				System.exit(0); // terminates program
 			}
 			
-			System.out.println( in );
-			
 			// get index of character
+			if ( in.contains( "+" ))
+			{
+				
+			}
+			
+			
 			int index = in.indexOf( "+" );
 			System.out.println( "Index of plus sign: " + index );
+			
+			String firstStr = in.substring(0 ,  index );
+			String secondStr = in.substring( index + 1 );
 			
 			// Chop up a sting
 			if (index != -1 )
 			{
-				String first = in.substring(0 ,  index );
-				String second = in.substring( index + 1 );
 				
-				System.out.println( first );
-				System.out.println( second );
+				System.out.println( firstStr );
+				System.out.println( secondStr );
 			}
 			
+			try
+			{
+				int first = Integer.parseInt( firstStr );
+				int second = Integer.parseInt( secondStr );
+				
+				System.out.println( "First int: " + first );
+				System.out.println( "Second int: " + second );
+				System.out.println( "answer is:  " + (first + second) );
 			
+			}
+			catch( Exception e )
+			{
+				System.out.println( "Error occured" );
+				continue;
+			}
+			
+			System.out.println( in );
 			
 		}
 	}

a30bb301245ae068ffb32d9d7bdd40ebed572a41
LUCAS MEHMEL <mehmell@my.canisius.edu>
2017-09-13 12:08:42 -0400

    Final program with bonus

--- Lab2/src./Main.java
+++ Lab2/src./Main.java
@@ -25,7 +25,7 @@ public class Main
 			else if ( in.contains( "+" ))
 			{
 				int index = in.indexOf( "+" );
-				System.out.println( "Index of plus sign: " + index );
+				//System.out.println( "Index of plus sign: " + index );
 				if (index == 0)
 				{
 					System.out.println( "ERROR: invalid number detected" );
@@ -68,10 +68,108 @@ public class Main
 				
 			}
 			
+			else if ( in.contains( "/" ))
+			{
+				int index = in.indexOf( "/" );
+				//System.out.println( "Index of division sign: " + index );
+
+				String firstStr = in.substring(0 ,  index );
+				String secondStr = in.substring( index + 1 );
+					
+				firstStr = firstStr.trim();
+				secondStr = secondStr.trim();
+					
+				//System.out.println( firstStr );
+				//System.out.println( secondStr );
+					
+				try
+				{
+					int first = Integer.parseInt( firstStr );
+					int second = Integer.parseInt( secondStr );
+						
+					if (first < 0)
+					{
+						System.out.println("ERROR: negative numbers are not allowed");
+						continue;
+					}
+					if (second < 0)
+					{
+						System.out.println("ERROR: negative numbers are not allowed");
+						continue;
+					}
+					else if (second == 0)
+					{
+						System.out.println("ERROR: dividing by zero is not allowed");
+						continue;
+					}
+					//System.out.println( "First int: " + first );
+					//System.out.println( "Second int: " + second );
+					System.out.println( "The result is:" + (first / second) );
+					
+				}
+				catch( Exception e )
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+					
+			}
+			
+			else if ( in.contains( "*" ))
+			{
+				int index = in.indexOf( "*" );
+				//System.out.println( "Index of multiplication sign: " + index );
+				if (index == 0)
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+				else if (index != -1 )
+				{
+					String firstStr = in.substring(0 ,  index );
+					String secondStr = in.substring( index + 1 );
+					
+					firstStr = firstStr.trim();
+					secondStr = secondStr.trim();
+					
+					//System.out.println( firstStr );
+					//System.out.println( secondStr );
+					
+					try
+					{
+						int first = Integer.parseInt( firstStr );
+						int second = Integer.parseInt( secondStr );
+						
+						if (first < 0)
+						{
+							System.out.println("ERROR: negative numbers are not allowed");
+							continue;
+						}
+						if (second < 0)
+						{
+							System.out.println("ERROR: negative numbers are not allowed");
+							continue;
+						}
+
+						//System.out.println( "First int: " + first );
+						//System.out.println( "Second int: " + second );
+						System.out.println( "The result is:" + (first * second) );
+					
+					}
+					catch( Exception e )
+					{
+						System.out.println( "ERROR: invalid number detected" );
+						continue;
+					}
+					
+				}
+				
+			}
+			
 			else if ( in.contains( "-" ))
 			{
 				int index = in.indexOf( "-" );
-				System.out.println( "Index of plus sign: " + index );
+				//System.out.println( "Index of plus sign: " + index );
 				
 				String firstStr = in.substring(0 ,  index );
 				String secondStr = in.substring( index + 1 );
@@ -94,7 +192,8 @@ public class Main
 					}
 						
 						//System.out.println( "First int: " + first );
-						//System.out.println( "Second int: " + second );
+						//q
+					System.out.println( "Second int: " + second );
 						System.out.println( "The result is:" + (first - second) );
 					
 					}
@@ -105,7 +204,7 @@ public class Main
 				}
 				
 				}
-			
+							
 			else
 			{
 				System.out.println( "ERROR: invalid or no operator specified - please try again");

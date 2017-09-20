bc4de14e64dfe701fed7f515aec94cf8499142cd
LUCAS MEHMEL <mehmell@my.canisius.edu>
2017-09-20 16:33:56 -0400

    Finished both LionImpl and PenguinImpl

--- Lab3/src/animals/LionImpl.java
+++ Lab3/src/animals/LionImpl.java
@@ -1,18 +1,23 @@
 package animals;
 
-public abstract class LionImpl implements Animal, Lion {
+public class LionImpl implements Animal, Lion 
+{
+	@Override
 	public void eat() {
 		System.out.println( "I'm a lion and I eat meat");
 	}
+	@Override
 	public void sleep() 
 	{
 		System.out.println( "I'm a lion and I sleep all day long" );
 		
 	}
+	@Override
 	public void roar()
 	{
 		System.out.println( "I'm a lion and HEAR ME ROAR" );
 	}
+	@Override
 	public void beTheKing()
 	{
 		System.out.println( "I'm a lion and I'm the king of the jungle" );

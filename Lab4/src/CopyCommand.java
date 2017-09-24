c5f10307195206890661dadc040bd30525531acb
LUCAS MEHMEL <mehmell@my.canisius.edu>
2017-09-24 16:16:22 -0400

    worked on execute part of the command

--- Lab4/src/CopyCommand.java
+++ Lab4/src/CopyCommand.java
@@ -45,7 +45,68 @@ public class CopyCommand {
 	}
 	public void execute()throws IOException
 	{
+		FileInputStream      fis = null;
+		BufferedInputStream  bis = null;
+		FileOutputStream     fos = null;
+		BufferedOutputStream bos = null;
+		
+		long fileSize = fileIn.length();
+		long numRead = 0;
+		long left = fileSize;
+
+		try
+		{
+			fis = new FileInputStream( fileIn );
+			bis = new BufferedInputStream( fis );
+			fos = new FileOutputStream( fileOut );
+			bos = new BufferedOutputStream( fos );
 			
+
+			
+			while ( left > 0 )
+			{
+				byte[] bytesWritten;
+				
+				if ( left > bufferSize )
+				{
+					bytesWritten = new byte[bufferSize];
+				}
+				else
+				{
+					bytesWritten = new byte[(int)left];
+				}
+				
+				numRead = bis.read(bytesWritten);
+				bos.write(bytesWritten, 0, bytesWritten.length);
+				
+				
+				left -= numRead;
+			}
+		
+			bos.flush();
+		
+		}
+		catch( IOException ioe)
+		{
+			ioe.printStackTrace (System.err );
+		}
+		
+		finally
+		{
+			if ( bis != null )
+				bis.close();
+				
+			if ( fis != null )
+				fis.close();
+			
+			if ( bos != null )
+				bos.close();
+				
+			if ( fos != null )
+				fos.close();
+		}
+
+		
 	}
 	
 }

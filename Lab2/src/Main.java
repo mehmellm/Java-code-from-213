b1950e37a1adbd955379df61da7ba11a74adfbe3
Benjamin Rood <benjaminjrood@gmail.com>
2017-09-24 20:24:06 -0400

    lab2: moved implementation to correct directory

--- Lab2/src/Main.java
+++ Lab2/src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
 	public static void main(String[] args )
 	{
-		String prompt = "Please enter a mathematical expression ( addition or substraction only, q to quit): ";
+		String prompt = "Please enter a mathematical expression (q to exit): ";
 		Scanner input = new Scanner(System.in );
 		
 		while(true)
@@ -16,50 +16,200 @@ public class Main
 			in = in.trim();
 			
 			// comparing entire string
-			if( in.equals( "q" ) ) // or .equalsIgnoreCase()
+			if( in.equalsIgnoreCase( "q" ) ) // or .equalsIgnoreCase()
 			{
 				System.out.println( "Goodbye!!! ");
 				System.exit(0); // terminates program
 			}
 			
-			// get index of character
-			if ( in.contains( "+" ))
+			else if ( in.contains( "+" ))
 			{
+				int index = in.indexOf( "+" );
+				//System.out.println( "Index of plus sign: " + index );
+				if (index == 0)
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+				else if (index != -1 )
+				{
+					String firstStr = in.substring(0 ,  index );
+					String secondStr = in.substring( index + 1 );
+					
+					firstStr = firstStr.trim();
+					secondStr = secondStr.trim();
+					
+					//System.out.println( firstStr );
+					//System.out.println( secondStr );
+					
+					try
+					{
+						int first = Integer.parseInt( firstStr );
+						int second = Integer.parseInt( secondStr );
+						
+						if (first < 0)
+						{
+							System.out.println("ERROR: negative numbers are not allowed");
+							continue;
+						}
+						
+						//System.out.println( "First int: " + first );
+						//System.out.println( "Second int: " + second );
+						System.out.println( "The result is:" + (first + second) );
+					
+					}
+					catch( Exception e )
+					{
+						System.out.println( "ERROR: invalid number detected" );
+						continue;
+					}
+					
+				}
 				
 			}
 			
-			
-			int index = in.indexOf( "+" );
-			System.out.println( "Index of plus sign: " + index );
-			
-			String firstStr = in.substring(0 ,  index );
-			String secondStr = in.substring( index + 1 );
-			
-			// Chop up a sting
-			if (index != -1 )
+			else if ( in.contains( "/" ))
 			{
-				
-				System.out.println( firstStr );
-				System.out.println( secondStr );
+				int index = in.indexOf( "/" );
+				//System.out.println( "Index of division sign: " + index );
+
+				String firstStr = in.substring(0 ,  index );
+				String secondStr = in.substring( index + 1 );
+					
+				firstStr = firstStr.trim();
+				secondStr = secondStr.trim();
+					
+				//System.out.println( firstStr );
+				//System.out.println( secondStr );
+					
+				try
+				{
+					int first = Integer.parseInt( firstStr );
+					int second = Integer.parseInt( secondStr );
+						
+					if (first < 0)
+					{
+						System.out.println("ERROR: negative numbers are not allowed");
+						continue;
+					}
+					if (second < 0)
+					{
+						System.out.println("ERROR: negative numbers are not allowed");
+						continue;
+					}
+					else if (second == 0)
+					{
+						System.out.println("ERROR: dividing by zero is not allowed");
+						continue;
+					}
+					//System.out.println( "First int: " + first );
+					//System.out.println( "Second int: " + second );
+					System.out.println( "The result is:" + (first / second) );
+					
+				}
+				catch( Exception e )
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+					
 			}
 			
-			try
+			else if ( in.contains( "*" ))
 			{
-				int first = Integer.parseInt( firstStr );
-				int second = Integer.parseInt( secondStr );
+				int index = in.indexOf( "*" );
+				//System.out.println( "Index of multiplication sign: " + index );
+				if (index == 0)
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+				else if (index != -1 )
+				{
+					String firstStr = in.substring(0 ,  index );
+					String secondStr = in.substring( index + 1 );
+					
+					firstStr = firstStr.trim();
+					secondStr = secondStr.trim();
+					
+					//System.out.println( firstStr );
+					//System.out.println( secondStr );
+					
+					try
+					{
+						int first = Integer.parseInt( firstStr );
+						int second = Integer.parseInt( secondStr );
+						
+						if (first < 0)
+						{
+							System.out.println("ERROR: negative numbers are not allowed");
+							continue;
+						}
+						if (second < 0)
+						{
+							System.out.println("ERROR: negative numbers are not allowed");
+							continue;
+						}
+
+						//System.out.println( "First int: " + first );
+						//System.out.println( "Second int: " + second );
+						System.out.println( "The result is:" + (first * second) );
+					
+					}
+					catch( Exception e )
+					{
+						System.out.println( "ERROR: invalid number detected" );
+						continue;
+					}
+					
+				}
 				
-				System.out.println( "First int: " + first );
-				System.out.println( "Second int: " + second );
-				System.out.println( "answer is:  " + (first + second) );
-			
 			}
-			catch( Exception e )
+			
+			else if ( in.contains( "-" ))
 			{
-				System.out.println( "Error occured" );
+				int index = in.indexOf( "-" );
+				//System.out.println( "Index of plus sign: " + index );
+				
+				String firstStr = in.substring(0 ,  index );
+				String secondStr = in.substring( index + 1 );
+					
+				firstStr = firstStr.trim();
+				secondStr = secondStr.trim();
+					
+				//System.out.println( firstStr );
+				//System.out.println( secondStr );
+					
+				try
+				{
+					int first = Integer.parseInt( firstStr );
+					int second = Integer.parseInt( secondStr );
+						
+					if (first < 0)
+					{
+						System.out.println("ERROR: negative numbers are not allowed");
+						continue;
+					}
+						
+						//System.out.println( "First int: " + first );
+						//q
+					System.out.println( "Second int: " + second );
+						System.out.println( "The result is:" + (first - second) );
+					
+					}
+				catch( Exception e )
+				{
+					System.out.println( "ERROR: invalid number detected" );
+					continue;
+				}
+				
+				}
+							
+			else
+			{
+				System.out.println( "ERROR: invalid or no operator specified - please try again");
 				continue;
 			}
-			
-			System.out.println( in );
 			
 		}
 	}

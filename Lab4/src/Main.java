2486f3b32ef212d13f36c77f8ae8a1c57756db00
LUCAS MEHMEL <mehmell@my.canisius.edu>
2017-09-25 14:26:50 -0400

    copy is added to the main

--- Lab4/src/Main.java
+++ Lab4/src/Main.java
@@ -8,5 +8,26 @@ public class Main {
 		String prompt = "Please enter command (copy | search | exit)";
 		Scanner input = new Scanner (System.in);
 		
+		while( true )
+		{
+			System.out.println( prompt );
+			System.out.print( "> ");
+			
+			String s = input.nextLine();
+			
+			if ( s.equals( "copy") )
+			{
+				System.out.println( "Please enter the path to the file to copy" );
+				System.out.print( "> ");
+				String fileIn = input.nextLine();
+				System.out.println( "Please enter the destination path" );
+				System.out.print( "> ");
+				String fileOut = input.nextLine();
+				System.out.println( "Please enter the size of the read buffer" );
+				System.out.print( "> ");
+				String bufferSize = input.nextLine();
+				CopyCommand( bufferSize, fileIn, fileOut);
+			}
+		}
 	}
 }

47af27ef7c4effef707dd85c3e0c73912ed3ee0e
LUCAS MEHMEL <mehmell@my.canisius.edu>
2017-09-26 18:01:26 -0400

    Search command is done working on bugs

--- Lab4/src/SearchCommand.java
+++ Lab4/src/SearchCommand.java
@@ -1,3 +1,60 @@
-public class SearchCommand {
+import java.io.*;
+
+
+
+public class SearchCommand 
+{
+	private File fileIn;
+	private String word;
 	
+	
+	public SearchCommand(File In, String term)
+	{
+		fileIn = In;
+		word = term;
+
+	}
+	
+	public boolean validateInputFile()
+	{
+		if (fileIn.exists())
+		{
+			if (fileIn.isDirectory())
+			{
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	public boolean execute() throws IOException
+	{
+
+		FileReader reader = new FileReader(fileIn);
+		BufferedReader buffReader = new BufferedReader(reader);
+		try
+		{
+			String ln = buffReader.readLine();
+			while ( ln != null)
+			{
+				if (ln.contains(word))
+				{
+					return true;
+				}
+
+			}
+		}
+		finally
+		{
+			if ( buffReader != null)
+			{
+				buffReader.close();
+			}
+			if ( reader != null)
+			{
+				reader.close();
+			}
+		}
+		return false;
+	}
 }
